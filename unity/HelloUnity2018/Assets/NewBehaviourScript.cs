15e8b35dc4a381ff328c247cb8f2bbb4fbff2065
UZ\hubin <232623741@qq.com>
2018-07-03 16:45:14 +0800

    unity init

---
+++ unity/HelloUnity2018/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿// using System;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	float a = 3;
+	uint b = 4;
+	// Use this for initialization
+	void Start () {
+		Debug.Log("cao");
+		Debug.Log("cao");
+		Debug.Log(a);
+		Debug.Log("cao");
+		Debug.Log("cao");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		a++;
+		if(a>100){
+			Debug.Log(a);
+			a = 0;
+		}
+	}
+}
