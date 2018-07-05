9856f761815ee11a6227295c0a1183be8ae8d339
UZ\hubin <232623741@qq.com>
2018-07-05 11:37:08 +0800

    小修改

--- src/pm/TestManager.go
+++ src/pm/TestManager.go
@@ -17,12 +17,18 @@ func (this *TestManager) RegisterFunction() {
 type C2L_M_TEST_1 struct{}
 
 func (this *C2L_M_TEST_1) execute(client *websocket.Conn, msg *Message) bool {
-	common.ProjectModeSort2Sort()
-	common.ModeLinkSort2Sort()
-	common.TplLinkSort2Sort()
-	test_manager.InsertSort()
+	// common.ProjectModeSort2Sort()
+	// common.ModeLinkSort2Sort()
+	// common.TplLinkSort2Sort()
+	// test_manager.InsertSort()
+	this.Test1()
 	return true
 }
 
-func (this *TestManager) InsertSort() {
+func (this *C2L_M_TEST_1) Test1() {
+	stmt, err := db.GetDb().Prepare(`UPDATE pm.pm_version SET name = 'aa' WHERE vid=122`)
+	defer stmt.Close()
+	db.CheckErr(err)
+	_, err = stmt.Exec()
+	db.CheckErr(err)
 }
