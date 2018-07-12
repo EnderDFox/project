67fe328d2470ae4dbb57d118f5b1ab098700fd8e
UZ\hubin <232623741@qq.com>
2018-07-12 10:30:36 +0800

    remove一些垃圾文件

--- src/test.sh
+++
@@ -1,4 +0,0 @@
-# sh /root/trunk/src/test.sh pm_amazing full r head
-
-proj_name=$1
-echo "[proj_name]:"$proj_name
--- src/test.sql
+++
@@ -1,3 +0,0 @@
-SELECT t1.*, t2.*
-FROM pm.pm_version AS t1, pm.pm_project AS t2
-WHERE 1=1
